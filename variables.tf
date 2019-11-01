049935574700f795821fa5410647d24acb6993fa
EC2 Default User <ec2-user@ip-10-0-0-135.eu-west-1.compute.internal>
2019-11-01 15:51:03 +0000

    upgrade to tf 0.12

--- variables.tf
+++ variables.tf
@@ -1,32 +1,33 @@
 variable "zone_id" {
-  type        = "string"
+  type        = string
   description = "Route53 zone hosting the domain MTA-STS/TLS-RPT is being deployed for."
 }
 
 variable "domain" {
-  type        = "string"
+  type        = string
   description = "The domain MTA-STS/TLS-RPT is being deployed for."
 }
 
 variable "mode" {
-  type        = "string"
+  type        = string
   default     = "testing"
   description = "MTA-STS policy 'mode'. Either 'testing' or 'enforce'."
 }
 
 variable "max_age" {
-  type        = "string"
+  type        = string
   default     = "86400"
   description = "MTA-STS max_age. Time in seconds the policy should be cached. Default is 1 day"
 }
 
 variable "mx" {
-  type        = "list"
+  type        = list(string)
   description = "'mx' value for MTA-STS policy. List of MX hostnames to be included in MTA-STS policy"
 }
 
 variable "reporting_email" {
-  type        = "string"
+  type        = string
   default     = ""
   description = "(Optional) Email to use for TLS-RPT reporting."
 }
+
