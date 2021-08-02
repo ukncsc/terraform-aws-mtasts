21fcd87bc5aaa2af3b3948e19c927a3297a4a296
EC2 Default User <ec2-user@ip-10-0-0-135.eu-west-1.compute.internal>
2021-08-02 07:31:09 +0000

    Switch to CloudFront

--- main.tf
+++ main.tf
@@ -1,12 +1,21 @@
+data "aws_caller_identity" "current" {}
+
 locals {
   policydomain = "mta-sts.${var.domain}"
   policyhash   = md5(format("%s%s%s", join("", var.mx), var.mode, var.max_age))
+  bucketname   = "${data.aws_caller_identity.current.account_id}.${var.domain}"
+}
+
+provider "aws" {
+  alias  = "useast1"
+  region = "us-east-1"
 }
 
 resource "aws_acm_certificate" "cert" {
   domain_name       = local.policydomain
   validation_method = "DNS"
   tags              = var.tags
+  provider          = aws.useast1
 }
 
 data "aws_route53_zone" "zone" {
@@ -24,120 +33,104 @@ resource "aws_route53_record" "cert_validation" {
 resource "aws_acm_certificate_validation" "cert" {
   certificate_arn         = aws_acm_certificate.cert.arn
   validation_record_fqdns = [aws_route53_record.cert_validation.fqdn]
+  provider                = aws.useast1
 }
 
-resource "aws_api_gateway_rest_api" "mtastspolicyapi" {
-  name = var.domain
-
-  endpoint_configuration {
-    types = ["REGIONAL"]
-  }
-  tags = var.tags
+resource "aws_s3_bucket" "policybucket" {
+  bucket = local.bucketname
+  acl    = "private"
 }
 
-resource "aws_api_gateway_resource" "wellknownresource" {
-  rest_api_id = aws_api_gateway_rest_api.mtastspolicyapi.id
-  parent_id   = aws_api_gateway_rest_api.mtastspolicyapi.root_resource_id
-  path_part   = ".well-known"
-}
-
-resource "aws_api_gateway_resource" "policyresource" {
-  rest_api_id = aws_api_gateway_rest_api.mtastspolicyapi.id
-  parent_id   = aws_api_gateway_resource.wellknownresource.id
-  path_part   = "mta-sts.txt"
-}
-
-resource "aws_api_gateway_method" "method" {
-  rest_api_id   = aws_api_gateway_rest_api.mtastspolicyapi.id
-  resource_id   = aws_api_gateway_resource.policyresource.id
-  http_method   = "GET"
-  authorization = "NONE"
-}
-
-resource "aws_api_gateway_integration" "integration" {
-  rest_api_id          = aws_api_gateway_rest_api.mtastspolicyapi.id
-  resource_id          = aws_api_gateway_resource.policyresource.id
-  http_method          = aws_api_gateway_method.method.http_method
-  type                 = "MOCK"
-  passthrough_behavior = "NEVER"
-
-  request_templates = {
-    "application/json" = <<EOF
-{
-   "statusCode" : 200
-}
-EOF
-
-  }
-}
-
-resource "aws_api_gateway_method_response" "twohundred" {
-  rest_api_id = aws_api_gateway_rest_api.mtastspolicyapi.id
-  resource_id = aws_api_gateway_resource.policyresource.id
-  http_method = aws_api_gateway_method.method.http_method
-  status_code = "200"
-}
-
-resource "aws_api_gateway_integration_response" "integrationresponse" {
-  rest_api_id = aws_api_gateway_rest_api.mtastspolicyapi.id
-  resource_id = aws_api_gateway_resource.policyresource.id
-  http_method = aws_api_gateway_method.method.http_method
-  status_code = aws_api_gateway_method_response.twohundred.status_code
-
-  # Transforms the backend JSON response to XML
-  response_templates = {
-    "text/plain" = <<EOF
-#set($context.responseOverride.header.Content-Type='text/plain')
+resource "aws_s3_bucket_object" "mtastspolicyfile" {
+  key          = ".well-known/mta-sts.txt"
+  bucket       = aws_s3_bucket.policybucket.id
+  content      = <<EOF
 version: STSv1
 mode: ${var.mode}
 ${join("", formatlist("mx: %s\n", var.mx))}max_age: ${var.max_age}
 EOF
-
-  }
-
-  depends_on = [aws_api_gateway_integration.integration]
+  content_type = "text/plain"
 }
 
-resource "aws_api_gateway_deployment" "deployment" {
-  depends_on = [aws_api_gateway_integration.integration]
+locals {
+  s3_origin_id = "myS3Origin"
+}
 
-  rest_api_id = aws_api_gateway_rest_api.mtastspolicyapi.id
-  stage_name  = "deployed"
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  origin {
+    domain_name = aws_s3_bucket.policybucket.bucket_regional_domain_name
+    origin_id   = local.s3_origin_id
 
-  # Include policyhash so there's a new deployment when the policy changes
-  variables = {
-    policyhash = local.policyhash
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.policybucketoai.cloudfront_access_identity_path
+    }
+  }
+  enabled = true
+
+  aliases = [local.policydomain]
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = local.s3_origin_id
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "https-only"
+    min_ttl                = 0
+    default_ttl            = 300
+    max_ttl                = 300
   }
 
-  lifecycle {
-    create_before_destroy = true
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    acm_certificate_arn      = aws_acm_certificate.cert.arn
+    ssl_support_method       = "sni-only"
+    minimum_protocol_version = "TLSv1.2_2018"
   }
 }
 
-resource "aws_api_gateway_domain_name" "domain" {
-  domain_name              = local.policydomain
-  regional_certificate_arn = aws_acm_certificate_validation.cert.certificate_arn
+resource "aws_cloudfront_origin_access_identity" "policybucketoai" {
+  comment = "OAI for MTA-STS policy bucket (${var.domain})"
+}
 
-  endpoint_configuration {
-    types = ["REGIONAL"]
+data "aws_iam_policy_document" "s3_policy" {
+  statement {
+    actions   = ["s3:GetObject"]
+    resources = ["${aws_s3_bucket.policybucket.arn}/*"]
+
+    principals {
+      type        = "AWS"
+      identifiers = [aws_cloudfront_origin_access_identity.policybucketoai.iam_arn]
+    }
   }
 }
 
-resource "aws_api_gateway_base_path_mapping" "basepathmapping" {
-  api_id      = aws_api_gateway_rest_api.mtastspolicyapi.id
-  stage_name  = aws_api_gateway_deployment.deployment.stage_name
-  domain_name = aws_api_gateway_domain_name.domain.domain_name
+resource "aws_s3_bucket_policy" "policybucketpolicy" {
+  bucket = aws_s3_bucket.policybucket.id
+  policy = data.aws_iam_policy_document.s3_policy.json
 }
 
-resource "aws_route53_record" "apigatewaypointer" {
-  name    = aws_api_gateway_domain_name.domain.domain_name
+resource "aws_route53_record" "cloudfrontalias" {
+  name    = local.policydomain
   type    = "A"
   zone_id = data.aws_route53_zone.zone.id
 
   alias {
     evaluate_target_health = true
-    name                   = aws_api_gateway_domain_name.domain.regional_domain_name
-    zone_id                = aws_api_gateway_domain_name.domain.regional_zone_id
+    name                   = aws_cloudfront_distribution.s3_distribution.domain_name
+    zone_id                = aws_cloudfront_distribution.s3_distribution.hosted_zone_id
   }
 }
 
@@ -163,4 +156,3 @@ resource "aws_route53_record" "mtastspolicydns" {
     "v=STSv1; id=${local.policyhash}",
   ]
 }
-
